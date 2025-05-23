ed115fd793d65689428643eb1067fe911c6ec7ba
Olingo Maxwell <69717524+olingomaxwell1999@users.noreply.github.com>
2025-05-23 20:06:45 +0300

    feat: implement admin dashboard page with DashboardPage component

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import DashboardPage from "../shared/Components/admin/dashboard/Dashboard";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      <DashboardPage />
+    </div>
+  );
 };
 
 export default page;
